36426fd053b80b813319ebc39a5331d89acc94f5
chibikes <chibuikem.okohsisi@gmail.com>
2026-03-18 10:40:18 +0100

    refactor: move `PostsBloc` provider to app root and configure `ApiClient` - wrap `MyApp` with `BlocProvider` in `main` to provide `PostsBloc` globally - initialize `ApiPostRepository` with an `ApiClient` instance and base URL - remove local `BlocProvider` from `MyApp` home property

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,19 @@ import 'package:chibuike_nerdbug_assessment/views/pages/posts_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'api_client.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    BlocProvider(
+      create: (context) => PostsBloc(
+        ApiPostRepository(
+          apiClient: ApiClient(baseUrl: 'https://jsonplaceholder.typicode.com'),
+        ),
+      )..add(GetPosts()),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -19,10 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: BlocProvider(
-        create: (context) => PostsBloc(ApiPostRepository())..add(GetPosts()),
-        child: const PostsPage(),
-      ),
+      home: const PostsPage(),
     );
   }
 }
