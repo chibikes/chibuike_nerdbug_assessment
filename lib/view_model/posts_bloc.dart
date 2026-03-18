e9fe90e894b551497a020fbf7987d87cf43dd7e1
chibikes <chibuikem.okohsisi@gmail.com>
2026-03-18 10:10:55 +0100

    chore: add `.gitignore` files to prevent versioning unwanted and build system and auto-generated files - add android `.gitignore` file - add ios `.gitignore` file - add flutter `.gitignore` file

---
+++ lib/view_model/posts_bloc.dart
@@ -0,0 +1,44 @@
+import 'package:bloc/bloc.dart';
+import 'package:chibuike_nerdbug_assessment/post_repository/post_repository.dart';
+import 'package:chibuike_nerdbug_assessment/view_model/posts_event.dart';
+import 'package:chibuike_nerdbug_assessment/view_model/posts_state.dart';
+
+import '../models/like_status.dart';
+import '../models/loading_status.dart';
+
+class PostsBloc extends Bloc<PostsEvent, PostsState> {
+  final PostRepository _postRepository;
+
+  PostsBloc(PostRepository postRepository)
+    : _postRepository = postRepository,
+      super(PostsState(loadingStatus: LoadingStatus.initial)) {
+    on<GetPosts>((event, emit) async {
+      emit(state.copyWith(loadingStatus: LoadingStatus.isLoading));
+      try {
+        final posts = await _postRepository.getPosts();
+        emit(
+          state.copyWith(loadingStatus: LoadingStatus.hasPosts, posts: posts),
+        );
+      } catch (e) {
+        print('something went wrong: $e');
+        emit(state.copyWith(loadingStatus: LoadingStatus.hasError));
+      }
+    });
+
+    on<LikePost>((event, emit) {
+      emit(state.copyWith(likedStatus: LikeStatus.isLiking));
+      final updatedPosts = state.posts.map((post) {
+        return post.id == event.id
+            ? post.copyWith(isLiked: !post.isLiked) // toggle
+            : post;
+      }).toList();
+
+      emit(
+        state.copyWith(
+          posts: [...updatedPosts],
+          likedStatus: LikeStatus.hasLiked,
+        ),
+      );
+    });
+  }
+}
